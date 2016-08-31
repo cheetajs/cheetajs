4e3ee580ff677ba1f11b0a96caee71c8e543237c
Danial Farid <danial.farid@gmail.com>
2016-08-31 12:04:31 +0300

    fixed sort

--- src/model.js
+++ src/model.js
@@ -17,7 +17,6 @@ $cheeta.Model = function (name, parent, modelRef) {
     }
     return this.modelRef;
   };
-
   this.getValue = function () {
     if (this.parent == null) {
       return window;
@@ -26,7 +25,7 @@ $cheeta.Model = function (name, parent, modelRef) {
       return parentVal == null ? parentVal : parentVal[this.names[0]];
     }
   };
-  this.setValue = function(val) {
+  this.setValue = function (val) {
     this.parent.getValue()[this.names[0]] = val;
   };
   this.child = function (name, modelRef, skipIntercept) {
@@ -40,131 +39,29 @@ $cheeta.Model = function (name, parent, modelRef) {
       if (val == null) {
         this.setValue({});
       }
-      if (!skipIntercept) model.intercept(this.getValue());
+      if (!skipIntercept) $cheeta.objectModel.intercept(model, this.getValue());
     }
     return model;
   };
 
-  this.addValueToModelMap = function (value) {
-    if (!value.$$cheetaId) {
-      value.$$cheetaId = $cheeta.Model.uid++;
-    }
-    if (!$cheeta.Model.objectIdModelMap[value.$$cheetaId]) {
-      $cheeta.Model.objectIdModelMap[value.$$cheetaId] = [];
-    }
-    $cheeta.Model.objectIdModelMap[value.$$cheetaId].push(this);
-  };
-  function fireValueChanges(parentVal, propName, prevVal) {
-    console.log('firing', parentVal, propName);
-    if (parentVal.$$cheetaId) {
-      var models = $cheeta.Model.objectIdModelMap[parentVal.$$cheetaId];
-      if (models) {
-        if (!models.length) {
-          delete $cheeta.Model.objectIdModelMap[parentVal.$$cheetaId];
-        } else {
-          for (var i = 0; i < models.length; i++) {
-            var childM = models[i].children[propName];
-            //todo may not be necessary to remove from models
-            if (!childM) {
-              console.error('WARNING no child model exists for fire value change', models[i].ref(), parentVal.$$cheetaId);
-              models.splice(i--, 1);
-              continue;
-            }
-            console.log('firing', childM.ref());
-            childM.valueChange(childM.getValue(), prevVal);
-          }
-        }
-      }
-    }
-  }
-
-  this.intercept = function (parentVal) {
-    var model = this;
-    if (parentVal != null) {
-      var propName = this.names[0];
-      var value;
-      var origVal = parentVal[propName];
-      console.log('intercepting: ', parentVal, propName, value);
-      Object.defineProperty(parentVal, propName, {
-        get: function () {
-          return value;
-        },
-        set: function (val) {
-          console.log('set: ', parentVal, propName, val);
-          if (val !== value) {
-            var prevVal = value;
-            value = val;
-            if (Object.isObject(val)) {
-              model.addValueToModelMap(val);
-            }
-            if (val !== undefined) {
-              for (var key in model.children) {
-                if (model.children.hasOwnProperty(key)) {
-                  // this.children[key].unintercept(this.prevValue);
-                  model.children[key].intercept(val);
-                }
-              }
-              if (Object.isArray(val)) {
-                for (var i = 0; i < $cheeta.Model.arrayMethodNames.length; i++) {
-                  var methodName = $cheeta.Model.arrayMethodNames[i];
-                  model.origArrayMethods[methodName] = val[methodName];
-                  val[methodName] = model.makeArrayFn(model, methodName);
-                }
-              }
-            }
-            fireValueChanges(parentVal, propName, prevVal);
-          }
-        },
-        enumerable: true,
-        configurable: true
-      });
-      model.setValue(origVal);
-    }
-  };
-
-  this.origArrayMethods = {};
-
-  this.makeArrayFn = function (model, methodName) {
-    return function () {
-      var oldLen = model.getValue().length, removedObjects;
-      try {
-        removedObjects = model.origArrayMethods[methodName].apply(this, arguments);
-        return removedObjects;
-      } finally {
-        model.valueChange();
-        if (removedObjects) {
-          for (var i = 0; i < removedObjects.length; i++) {
-            var obj = removedObjects[i];
-            delete $cheeta.Model.objectIdModelMap[obj.$$cheetaId];
-          }
-        }
-        var newLen = model.getValue().length;
-        if (oldLen !== newLen) {
-          var lengthModel = model.child('length', null, true);
-          lengthModel.valueChange(newLen, oldLen);
-        }
-      }
-    };
-  };
   this.valueChange = function () {
     document.dispatchEvent(new CustomEvent('Oo-model-change-' + this.ref() + (this.refId || ''),
-      {'detail': {/*value: val, prevValue: prevVal, */target: this}}));
+      {'detail': {value: this.value, prevValue: this.prevValue, target: this}}));
     return this;
   };
   this.watch = function (elem, callback) {
-    // var model = this;
-    var listener, modelRef = this.ref();
-    document.addEventListener('Oo-model-change-' + modelRef, (listener = function () {
-      callback.call(elem/*, e.detail.value, e.detail.prevValue*/);
+    var model = this;
+    var listener, modelRef = this.ref() + (this.refId || '');
+    document.addEventListener('Oo-model-change-' + modelRef, (listener = function (e) {
+      callback.call(elem, e.detail.value, e.detail.prevValue);
     }), false);
     elem.addEventListener('removed', function () {
       document.removeEventListener('Oo-model-change-' + modelRef, listener);
+      delete model.parent.children[model.names[0]];
     }, false);
     this.listeners.push(listener);
   };
-  this.unwatch = function(elem) {
-    elem.dispatchEvent(new CustomEvent('removed', {'detail': {target: elem}}));
-  };
+
   // this.interceptPropProxy = function (value, name, skipDefine) {
   //   if (!skipDefine) {
   //     var handler = {
@@ -187,9 +84,110 @@ $cheeta.Model = function (name, parent, modelRef) {
   //   }
   // };
 };
-$cheeta.Model.objectIdModelMap = {};
-$cheeta.Model.uid = 2;
-$cheeta.Model.arrayMethodNames = ['push', 'pop', 'shift', 'unshift', 'splice', 'reverse'];
+$cheeta.objectModel = {
+  objectIdModelMap: {},
+  uid: 2,
+  arrayMethodNames: ['push', 'pop', 'shift', 'unshift', 'splice', 'reverse'],
+  origArrayMethods: {},
+  addValueToModelMap: function (value, model) {
+    if (!value.$$cheetaId) {
+      value.$$cheetaId = this.uid++;
+    }
+    if (!this.objectIdModelMap[value.$$cheetaId]) {
+      this.objectIdModelMap[value.$$cheetaId] = [];
+    }
+    this.objectIdModelMap[value.$$cheetaId].push(model);
+  },
+  fireValueChanges: function (parentVal, propName) {
+    console.log('firing', parentVal, propName);
+    if (parentVal.$$cheetaId) {
+      var models = this.objectIdModelMap[parentVal.$$cheetaId];
+      if (models) {
+        if (!models.length) {
+          delete this.objectIdModelMap[parentVal.$$cheetaId];
+        } else {
+          for (var i = 0; i < models.length; i++) {
+            var m = models[i].children[propName];
+            if (!m) {
+              models.splice(i--, 1);
+              continue;
+            }
+            console.log('firing', m.ref());
+            m.valueChange();
+          }
+        }
+      }
+    }
+  },
+  makeArrayFn: function (model, methodName) {
+    return function () {
+      var oldLen = model.getValue().length;
+      try {
+        return model.origArrayMethods[methodName].apply(this, arguments);
+      } finally {
+        model.valueChange();
+        var newLen = model.getValue().length;
+        if (newLen !== oldLen) {
+          if (newLen < oldLen) {
+            for (var i = newLen; i < oldLen; i++) {
+              var child = model.child(i);
+              child.prevValue = child.value;
+              child.value = undefined;
+              // child.valueChange();
+              delete model.children[child.names[0]];
+            }
+          }
+          var lengthModel = model.child('length', null, true);
+          lengthModel.prevValue = oldLen;
+          lengthModel.value = newLen;
+          lengthModel.valueChange();
+        }
+      }
+    };
+  },
+  intercept: function (model, parentVal) {
+    var objModel = this;
+    if (parentVal != null) {
+      var propName = model.names[0];
+      var value;
+      var origVal = parentVal[propName];
+      console.log('intercepting: ', parentVal, propName, value);
+      Object.defineProperty(parentVal, propName, {
+        get: function () {
+          return value;
+        },
+        set: function (val) {
+          console.log('set: ', parentVal, propName, val);
+          if (val !== value) {
+            value = val;
+            if (Object.isObject(val)) {
+              objModel.addValueToModelMap(val, model);
+            }
+            if (val !== undefined) {
+              for (var key in model.children) {
+                if (model.children.hasOwnProperty(key)) {
+                  // this.children[key].unintercept(this.prevValue);
+                  objModel.intercept(model.children[key], val);
+                }
+              }
+              if (Object.isArray(val)) {
+                for (var i = 0; i < objModel.length; i++) {
+                  var methodName = objModel.arrayMethodNames[i];
+                  model.origArrayMethods[methodName] = val[methodName];
+                  val[methodName] = objModel.makeArrayFn(model, methodName);
+                }
+              }
+            }
+            objModel.fireValueChanges(parentVal, propName);
+          }
+        },
+        enumerable: true,
+        configurable: true
+      });
+      model.setValue(origVal);
+    }
+  }
+};
 
 //$cheeta.refresh = function(modelRef) {
 //	var model = $cheeta.model.createOrGetModel(null, modelRef);
